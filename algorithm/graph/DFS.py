d43bb392d2e7b483c963532a5b06ad9e53c4722b
0happydeath0 <0happydeath0@naver.com>
2022-10-06 13:29:59 +0900

    DFS

---
+++ algorithm/graph/DFS.py
@@ -0,0 +1,33 @@
+# 반복문을 이용한 dfs
+
+def dfs(v):
+    visited[v] = True  # 시작 정점 방문 처리
+    stack = [v]
+    print(v, end=' ')
+
+    while stack:  # 스택이 빌 때까지 반복
+        for next_v in graph[v]:  # 인접한 모든 정점에 대해
+            if not visited[next_v]:  # 아직 방문하지 않았다면
+                v = next_v  # 이동
+                visited[v] = True  # 방문 처리
+                stack.append(v)  # 스택에 넣기
+                print(v, end=' ')
+                break
+        else:  # 더이상 방문할 인접 정점이 없다면
+            v = stack.pop()
+
+
+n, m = map(int, input().split())  # 정점, 간선 개수
+edges = list(map(int, input().split()))  # 간선 정보
+graph = [[] for _ in range(n + 1)]  # 그래프 -> n + 1인 이유는 정점 번호가 1번부터이기 때문
+visited = [False] * (n + 1)  # 방문 처리 리스트 -> n + 1인 이유는 정점 번호가 1번부터이기 때문
+
+# 인접 그래프 만들기
+for i in range(0, len(edges), 2):
+    v1, v2 = edges[i], edges[i + 1]  # 인접한 두 정점
+    # v1, v2를 양방향으로 넣는 이유는, 화살표가 없는 무방향 그래프이기 때문
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+dfs(1)  # 시작 정점을 1로 탐색 시작
+
